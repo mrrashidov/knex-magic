6240a5280fc13b3a389d054aa9b731051c5125ac
Shoxrux Rashidov <mrrashidov@hotmail.com>
2025-06-04 23:36:08 +0500

    refactor: remove Knex-related files and interfaces for cleanup

--- lib/common/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./knex.decorator";
-export * from "./knex.util";
--- lib/knex.constants.ts
+++
@@ -1,2 +0,0 @@
-export const KNEX_MODULE_OPTIONS = "KnexModuleOptions";
-export const DEFAULT_CONNECTION_NAME = "default";
