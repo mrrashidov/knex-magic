a931d2b9495bc2d71797b5be65c7f1ee10f1063c
Shoxrux Rashidov <mrrashidov@hotmail.com>
2025-06-05 02:38:28 +0500

    chore: format jest.config.js for improved readability and consistency

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/test'],
-    testMatch: ['**/*.test.ts'],
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/lib/$1'
-    },
-    coverageDirectory: 'coverage',
-    collectCoverageFrom: [
-      'lib/**/*.ts',
-      '!lib/**/*.d.ts'
-    ]
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/lib/$1'
+  },
+  setupFilesAfterEnv: ['<rootDir>/test/helpers/setupTests.ts'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'lib/**/*.ts',
+    '!lib/**/*.d.ts'
+  ],
+  verbose: true,
+  testTimeout: 30000,
+  maxWorkers: 1
+};
