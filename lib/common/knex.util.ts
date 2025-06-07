a411923073784993799db73b9cfc530bc1b7126a
Shoxrux Rashidov <mrrashidov@hotmail.com>
2025-06-07 11:05:39 +0500

    fix: refactor retry logic in handleRetry function for improved error handling

--- lib/common/knex.util.ts
+++ lib/common/knex.util.ts
@@ -1,6 +1,6 @@
 import { DEFAULT_CONNECTION_NAME } from "../knex.constants";
 import { KnexModuleOptionsI } from "../interfaces";
-import { delay, Observable, retryWhen, scan } from "rxjs";
+import { Observable, retry, timer } from "rxjs";
 import { Logger } from "@nestjs/common";
 
 const logger = new Logger("KnexModule");
@@ -15,27 +15,25 @@ export function getConnectionToken(
 }
 
 export function handleRetry(
-  retryAttempts = 9,
+  maxAttempts = 9,
   retryDelay = 3000
 ): <T>(source: Observable<T>) => Observable<T> {
   return <T>(source: Observable<T>) =>
     source.pipe(
-      retryWhen((e) =>
-        e.pipe(
-          scan((errorCount, error: Error) => {
-            logger.error(
-              `Unable to connect to the database. Retrying (${
-                errorCount + 1
-              })...`,
-              error.stack
-            );
-            if (errorCount + 1 >= retryAttempts) {
-              throw error;
-            }
-            return errorCount + 1;
-          }, 0),
-          delay(retryDelay)
-        )
-      )
+      retry({
+        count: maxAttempts,
+        delay: (error: Error, retryCount: number) => {
+          logger.error(
+            `Unable to connect to the database. Retrying (${retryCount})...`,
+            error.stack
+          );
+          
+          if (retryCount >= maxAttempts) {
+            throw error;
+          }
+          
+          return timer(retryDelay);
+        },
+      })
     );
 }
